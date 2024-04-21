b51a743a3293f27b396bc21540bc9f4fd3067fc4
Erik Rivera <erik.river@gmail.com>
2024-04-20 19:16:27 -0600

    added test for files

---
+++ tests/utils/test_files.py
@@ -0,0 +1,42 @@
+import pytest
+
+from cookieplone.utils import files
+
+
+@pytest.fixture
+def tmp_files(tmp_path):
+    folder1 = tmp_path / "test" / ".github"
+    folder1.mkdir(parents=True)
+
+    file1 = tmp_path / "test" / "test_file.txt"
+    file1.touch()
+
+    return file1
+
+
+def test_remove_files(tmp_path, tmp_files):
+    files_to_remove = tmp_files
+    folders_to_remove = tmp_files.parent
+
+    assert files_to_remove.exists()
+    files.remove_files(tmp_path, [files_to_remove])
+    assert not files_to_remove.exists()
+
+    assert folders_to_remove.exists()
+    files.remove_files(tmp_path, [folders_to_remove])
+    assert not folders_to_remove.exists()
+
+
+def test_remove_files_nonexistent_file(tmp_path):
+    files_to_remove = ["nonexistent_file.txt"]
+    base_path = tmp_path
+    assert files.remove_files(base_path, files_to_remove) == None
+
+
+def test_remove_gha(tmp_files):
+    base_path = tmp_files.parent
+
+    gha = base_path / ".github"
+    assert gha.exists()
+    files.remove_gha(base_path)
+    assert not gha.exists()
