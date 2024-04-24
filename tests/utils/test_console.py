d2f91ed6fde621004dd37d55c2bf1766176c0022
Erik Rivera <erik@rivera.pro>
2024-04-24 14:36:58 -0600

    Added tests for utils (#7)
    * added test for files
    
    * added test for git
    
    * added function alias
    
    * added tests for console
    
    * added tests for containers
    
    * added tests for internal

---
+++ tests/utils/test_console.py
@@ -0,0 +1,25 @@
+import pytest
+from unittest.mock import patch
+
+from cookieplone.utils import console
+
+
+@patch("cookieplone.utils.console.print")
+def test_print_plone_banner(mock_print):
+    console.print_plone_banner()
+    mock_print.assert_called_once_with(console.BANNER, "bold", "blue")
+
+
+@pytest.mark.parametrize(
+    "func,msg,style,color",
+    [
+        [console.info, "foo", "bold", "white"],
+        [console.success, "foo", "bold", "green"],
+        [console.error, "foo", "bold", "red"],
+        [console.warning, "foo", "bold", "yellow"],
+    ],
+)
+@patch("cookieplone.utils.console.print")
+def test_prints(mock_print, func, msg, style, color):
+    func(msg)
+    mock_print.assert_called_once_with(msg, style, color)
