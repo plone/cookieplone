a2e0f8ca9f97b5056f8c7be8f48a87c0b6cc44f6
Érico Andrei <ericof@gmail.com>
2024-05-17 10:46:16 +0200

    Small fixes to cookieplone/utils/plone.py functions

--- cookieplone/utils/plone.py
+++ cookieplone/utils/plone.py
@@ -5,8 +5,14 @@ def add_dependency_profile_to_metadata(profile: str, raw_xml: str) -> str:
     """Inject a dependency into the metadata.xml file."""
     data = xmltodict.parse(raw_xml)
     if not data["metadata"].get("dependencies"):
-        data["metadata"]["dependencies"] = []
-    data["metadata"]["dependencies"].append({"dependency": f"profile-{profile}"})
+        data["metadata"]["dependencies"] = {"dependency": []}
+    elif "dependency" not in data["metadata"].get("dependencies", {}):
+        data["metadata"]["dependencies"]["dependency"] = []
+    elements = data["metadata"]["dependencies"]["dependency"]
+    if isinstance(elements, str):
+        elements = [elements]
+    elements.append(f"profile-{profile}")
+    data["metadata"]["dependencies"]["dependency"] = elements
     raw_xml = xmltodict.unparse(data, short_empty_elements=True, pretty=True)
     return raw_xml
 
@@ -14,7 +20,7 @@ def add_dependency_profile_to_metadata(profile: str, raw_xml: str) -> str:
 def add_dependency_to_zcml(package: str, raw_xml: str) -> str:
     """Inject a dependency into the dependencies.zcml file."""
     data = xmltodict.parse(raw_xml, process_namespaces=False)
-    if "include" not in data["configure"]:
+    if not data["configure"].get("include"):
         data["configure"]["include"] = []
     data["configure"]["include"].append({"@package": f"{package}"})
     raw_xml = xmltodict.unparse(data, short_empty_elements=True, pretty=True)
