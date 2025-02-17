859f7ecdaed73c518e4a88068d74eb1dc3f998bb
Mikel Larreategi <mlarreategi@codesyntax.com>
2025-02-17 11:48:38 +0100

    feat: new filter to create the docker image separator

--- cookieplone/utils/containers.py
+++ cookieplone/utils/containers.py
@@ -2,8 +2,13 @@
 #
 # SPDX-License-Identifier: MIT
 REGISTRIES = {"docker_hub": "", "github": "ghcr.io/", "gitlab": "registry.gitlab.com/"}
+SEPARATORS = {"docker_hub": "-", "github": "-", "gitlab": "/"}
 
 
 def image_prefix(registry: str) -> str:
     """Return the image prefix to be used based on the registry used."""
     return REGISTRIES.get(registry, "")
+
+def image_separator(registry: str) -> str:
+    """ Return the image separator to be used based on the registry used. """
+    return SEPARATORS.get(registry, "-")
