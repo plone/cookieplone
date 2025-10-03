f0af1fbca64b48118df881ebcd0bdc93a4661b84
Érico Andrei <ericof@gmail.com>
2025-10-03 13:33:58 -0300

    Use a compacter logo (Closes #112)

--- tests/utils/test_console.py
+++ tests/utils/test_console.py
@@ -5,10 +5,35 @@ import pytest
 from cookieplone.utils import console
 
 
+@pytest.fixture
+def set_console_width(monkeypatch):
+    import os
+
+    def func(width: int) -> None:
+        monkeypatch.setattr(
+            "os.get_terminal_size", lambda: os.terminal_size((width, 80))
+        )
+
+    return func
+
+
 @patch("cookieplone.utils.console.print")
 def test_print_plone_banner(mock_print):
     console.print_plone_banner()
     mock_print.assert_called_once_with(console.BANNER, "bold", "blue")
+
+
+@pytest.mark.parametrize(
+    "width,banner",
+    [
+        (80, console.BANNER),
+        (90, console.PLONE_LOGOTYPE_BANNER),
+        (100, console.PLONE_LOGOTYPE_BANNER),
+    ],
+)
+def test_choose_banner(set_console_width, width: int, banner: str):
+    set_console_width(width)
+    assert console.choose_banner() == banner
 
 
 @pytest.mark.parametrize(
