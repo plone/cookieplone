f93904bdb0f7ca1330e52fbf193978cfee2737c3
Érico Andrei <ericof@gmail.com>
2026-03-17 12:25:39 +0200

    Add tests to get_password_from_env

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -43,3 +43,19 @@ def test_validate_extra_context_fail(value: list[str] | None, expected: str):
     with pytest.raises(BadParameter) as exc:
         func(value)
     assert expected in str(exc)
+
+
+@pytest.mark.parametrize(
+    "env_var,value,expected",
+    (
+        ("FOO", "bar", ""),
+        ("COOKIEPLONE_REPO_PASSWORD", "bar", "bar"),
+        ("COOKIECUTTER_REPO_PASSWORD", "foo", "foo"),
+    ),
+)
+def test_get_password_from_env(monkeypatch, env_var: str, value: str, expected: str):
+    """Test get_password_from_env."""
+    monkeypatch.setenv(env_var, value)
+    func = cli.get_password_from_env
+    result = func()
+    assert result == expected
