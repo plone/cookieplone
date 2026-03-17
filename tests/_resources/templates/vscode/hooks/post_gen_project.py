4ad84c4ec8c8253943c31bb3ae0a8f085a728688
Érico Andrei <ericof@gmail.com>
2026-03-17 17:14:18 +0200

    Reimplement the fixtures from pytest-cookies to use the new cookieplone and schemas

---
+++ tests/_resources/templates/vscode/hooks/post_gen_project.py
@@ -0,0 +1,14 @@
+"""Post generation hook."""
+
+from collections import OrderedDict
+
+context: OrderedDict = {{cookiecutter}}
+
+
+def main():
+    """Final fixes."""
+    pass
+
+
+if __name__ == "__main__":
+    main()
