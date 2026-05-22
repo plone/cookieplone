258ffecc2378d0af352a05838ecc9ba44711319a
Mikel Larreategi <mlarreategi@codesyntax.com>
2026-05-22 11:24:02 +0200

    tests

--- tests/settings/test_plone_python.py
+++ tests/settings/test_plone_python.py
@@ -15,27 +15,32 @@ def test_plone_61_exists():
     assert "6.1" in settings.PLONE_PYTHON
 
 
-@pytest.mark.parametrize("version", ["6.0", "6.1"])
+def test_plone_62_exists():
+    """Plone 6.2 has Python version support defined."""
+    assert "6.2" in settings.PLONE_PYTHON
+
+
+@pytest.mark.parametrize("version", ["6.0", "6.1", "6.2"])
 def test_entries_are_python_version_support(version: str):
     """Each entry is a PythonVersionSupport instance."""
     assert isinstance(settings.PLONE_PYTHON[version], settings.PythonVersionSupport)
 
 
-@pytest.mark.parametrize("version", ["6.0", "6.1"])
+@pytest.mark.parametrize("version", ["6.0", "6.1", "6.2"])
 def test_earliest_in_supported(version: str):
     """Earliest version is in the supported list."""
     pvs = settings.PLONE_PYTHON[version]
     assert pvs.earliest in pvs.supported
 
 
-@pytest.mark.parametrize("version", ["6.0", "6.1"])
+@pytest.mark.parametrize("version", ["6.0", "6.1", "6.2"])
 def test_latest_in_supported(version: str):
     """Latest version is in the supported list."""
     pvs = settings.PLONE_PYTHON[version]
     assert pvs.latest in pvs.supported
 
 
-@pytest.mark.parametrize("version", ["6.0", "6.1"])
+@pytest.mark.parametrize("version", ["6.0", "6.1", "6.2"])
 def test_earliest_before_latest(version: str):
     """Earliest version appears before latest in the supported list."""
     pvs = settings.PLONE_PYTHON[version]
